86418b8d15a71a6e04b8897340b3675f50ef9d07
lifthus <lifthus531@gmail.com>
2022-11-20 17:25:25 +0900

    working on python model

--- predModel/model24.py
+++ predModel/model24.py
@@ -1,15 +1,22 @@
 import sys
 import json
 
+import tensorflow as tf
+
 import preprcs
 
 with open('./predModel/model24Input.json', 'r') as f:
   data = json.load(f)
 
-data = preprcs(data)
+data = preprcs.preprcs(data)
 
 with open('./predModel/model24Output.json', 'w') as f:
-  json.du
-  
+  json.dump(data, f)
+
+# Prediction for every methods 
+# ============================
+
+# ============================
+
 print('Amethod 183 105 205\nBmethod 200 100 302\nCmethod 200 100 302\nDmethod 200 100 302\nEmethod 200 100 302',end='')
 sys.stdout.flush()
