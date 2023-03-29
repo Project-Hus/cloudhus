0a6b9c738a11338e4e8fbaae59d23652288ae354
lifthus <lifthus531@gmail.com>
2023-03-29 23:29:33 +0900

    working on 404 issue and cors issue

--- services/hus-auth/main.go
+++ services/hus-auth/main.go
@@ -28,7 +28,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var echoLambda *echoadapter.EchoLambda
+var echoLambda *echoadapter.EchoLambdaV2
 var dbClient *ent.Client
 
 // @title Project-Hus auth server
@@ -42,7 +42,7 @@ var dbClient *ent.Client
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @host lifthus.com
 // @BasePath /auth
-func init() {
+func main() {
 	// set .env
 	//err = godotenv.Load()
 	//if err != nil {
@@ -69,8 +69,11 @@ func init() {
 		// then all these headers need to be applied in API Gateway level also.
 		AllowOrigins: hus.Origins,
 
+		// to allow all headers
 		AllowHeaders: []string{
 			echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization,
+			echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowHeaders, echo.HeaderAccessControlAllowMethods,
+			echo.HeaderXRequestedWith,
 		},
 		AllowCredentials: true,
 		AllowMethods: []string{
@@ -108,23 +111,27 @@ func init() {
 	// provide api docs with swagger 2.0
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
-	echoLambda = echoadapter.New(e)
+	echoLambda = echoadapter.NewV2(e)
 
 	// Run the server
 	//e.Logger.Fatal(e.Start(":9090"))
+
+	lambda.Start(Handler)
 }
 
 type Host struct {
 	Echo *echo.Echo
 }
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	resp, err := echoLambda.ProxyWithContext(ctx, req)
-	fmt.Println("RESPONSE==========")
+	// get request host and path
+	hst := req.Headers["Host"]
+	// get path from req
+	pth := req.RequestContext.HTTP.Path
+	// get origin from req
+	org := req.Headers["Origin"]
+	fmt.Println("RESPONSE==========", hst, pth, org)
 	fmt.Println(fmt.Sprintf("%+v", resp))
 	return resp, err
-}
-func main() {
-	// it blocks and never returns
-	lambda.Start(Handler)
 }
